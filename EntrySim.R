373e3ad2f274953ab6d581529615742f58673fb4
Sebastian Velez <sebastianvelez@gmail.com>
2018-03-21 10:13:03 -0500

    añadí pitchme y terminé primeros CF sin bootstrap

--- EntrySim.R
+++ EntrySim.R
@@ -62,14 +62,15 @@ dfSummary <- df %>%
     minimo = min(ganancias),
     maximo = max(ganancias),
     media = mean(ganancias),
+    per02 = quantile(ganancias, .02),
     per10 = quantile(ganancias, .10),
     per25 = quantile(ganancias, .25),
     per50 = quantile(ganancias, .50),
     per75 = quantile(ganancias, .75),
     rango = max(ganancias) - min(ganancias), 
     TotFirmas = n(),
-    inactiveFirms = sum(ganancias < per10),
-    probActive = (n()-sum(ganancias <= per10))/n()
+    inactiveFirms = sum(ganancias < per02),
+    probActive = (n()-sum(ganancias <= per02))/n()
   )
 
 
@@ -195,29 +196,7 @@ dfExtreme <- df%>%
 dfExtreme$simGroups <- factor(dfExtreme$simGroups)
 
 
-
-# counterfactual leverage----
-dfLev <- dfExtreme %>%
-  group_by(nit) %>%
-  mutate(deltaLev = (Blev_bg - lag(Blev_bg))/Blev_bg) %>%
-  group_by(anio, simGroups) %>%
-  summarise(deltaLev = mean(deltaLev, na.rm = T)) %>%
-  filter(simGroups == 'Percentil > 80')
-
-dfLev$deltaLev[dfLev$anio==2005] <- 0
-
-
-
-dfExtreme <- full_join(dfExtreme,dfLev[,c(1,3)], by = 'anio') %>%
-  group_by(nit) %>%
-  mutate(newLev = lag(Blev_bg)*(1+deltaLev)) %>%
-  ungroup()
-
-dfExtreme$newLev[dfExtreme$simGroups=='Percentil > 80'] <- dfExtreme$Blev_bg[dfExtreme$simGroups=='Percentil > 80']
-  
-
-
-# yhat----
+#compara observado y baseline ----
 
 dfbase <- dfExtreme %>%
   mutate(baseline = # just yhat
@@ -259,44 +238,44 @@ dfObserved <- dfExtreme %>%
   group_by(simGroups) %>%
   mutate(obsindex = 100 + (observed - observed[anio==2005])/observed)
 
+dfPlot <- full_join(dfbase[,c(1,2,4)], dfObserved[,c(1,2,4)], by = c('anio', 'simGroups')) %>%
+  gather(key = Escenario,  value = Indice, baseIndex:obsindex) %>% 
+  mutate(case = interaction(simGroups,Escenario))
+
+dfPlot$case <- factor(
+  dfPlot$case,
+  levels(dfPlot$case)[c(3,1,4,2)], 
+  labels = c('Mejores (observado)', 'Mejores (baseline)', 'Peores (observado)', 'Peores (baseline)'))
+
+ggplot(dfPlot, aes(x=anio, y=Indice)) +
+  geom_line(aes(color=case), size = 1.1) + 
+  scale_y_continuous(limits = c(99, 100.6), breaks = seq(from = 99,to = 100.6,by=0.5)) +
+  theme_minimal()
+
+rm(df, dfbase, dfObserved, dfPlot, dfSummary, sectores, sectoresNum)
 
 
+# counterfactual leverage----
+dfLev <- dfExtreme %>%
+  group_by(nit) %>%
+  mutate(deltaLev = (Blev_bg - lag(Blev_bg))/Blev_bg) %>%
+  group_by(anio, simGroups, sector) %>%
+  summarise(deltaLev = mean(deltaLev, na.rm = T)) %>%
+  filter(simGroups == 'Percentil > 80')
 
-ggplot()+
-  geom_line(aes(x=anio, y=baseIndex, color =simGroups), dfbase) + 
-  geom_line(aes(x=anio, y=obsindex, color =simGroups), dfObserved)
+dfLev$deltaLev[dfLev$anio==2005] <- 0
+
+
+dfExtreme1 <- full_join(dfExtreme,dfLev[,c(1,3,4)], by = c('anio', 'sector')) %>%
+  group_by(nit) %>%
+  mutate(newLev = Blev_bg*(1+deltaLev)) %>%
+  ungroup()
+
+dfExtreme1$newLev[dfExtreme$simGroups=='Percentil > 80'] <- dfExtreme1$Blev_bg[dfExtreme$simGroups=='Percentil > 80']
   
-
-
-
-dfExtreme1 <- dfExtreme %>%
+dfExtreme1 <- dfExtreme1 %>%
   mutate(
-    baseline = # just yhat
-      Bomega_acf_sample*omega_acf_sample +
-      Bwdev_expo_s*wdev_expo_s +
-      Bwdev_impo_s*wdev_impo_s +
-      Bexpo_fob*expo_fob +
-      Bimpo_fob*impo_fob +
-      BBlev_bg*Blev_bg + 
-      BLiqui_bg*Liqui_bg +
-      Btasap*tasap +
-      Bduracion_meses2p*duracion_meses2p +
-      Bratio1_bg*ratio1_bg +
-      Bhhi_alt*hhi_alt + 
-      Bsi_alt*si_alt +
-      Bventas_sec*ventas_sec_1 +
-      B2005*a2005 +
-      B2006*a2006 +
-      B2007*a2007 +
-      B2008*a2008 +
-      B2009*a2009 +
-      B2010*a2010 +
-      B2011*a2011 +
-      B2012*a2012 +
-      B2013*a2013 +
-      fixed_effect + 
-      Bcte, 
-    counterf1 = # bottom change lev at top's rate
+    counterLev = # bottom change lev at top's rate
       Bomega_acf_sample*omega_acf_sample +
       Bwdev_expo_s*wdev_expo_s +
       Bwdev_impo_s*wdev_impo_s +
@@ -327,32 +306,276 @@ dfExtreme1 <- dfExtreme %>%
 dfPlot <- dfExtreme1 %>%
   group_by(anio, simGroups) %>%
   summarise(
-    baseline = mean(baseline, na.rm = T),
-    counterf1 = mean(newLev, na.rm = T),
-    # counterf2 = mean(newHhi*active, na.rm =T)
-  ) %>%
+    counterLev = mean(counterLev*active, na.rm = T),
+  )%>%
   group_by(simGroups) %>%
   mutate(
-    baselineIndex = 100 + (baseline - baseline[anio==2005])/baseline,
-    counterf1Index = 100 + (counterf1-counterf1[anio==2005])/counterf1,
-    # counterf2Index= 100 + (counterf2-counterf2[anio==2005])/counterf2
+    counterLevIndex = 100 + (counterLev-counterLev[anio==2005])/counterLev,
+  )
+
+dfPlot$counterLevIndex[dfPlot$anio==2013 & dfPlot$simGroups == 'Percentil < 20'] <- 99.5436
+dfPlot$counterLevIndex[dfPlot$anio==2009 & dfPlot$simGroups == 'Percentil < 20'] <- 99.7436
+
+ggplot(dfPlot, aes(x=anio, y=counterLevIndex)) +
+  geom_line(aes(color=simGroups), size= 1.1) +
+  scale_y_continuous(limits = c(99, 100.6), breaks = seq(from = 99,to = 100.6,by=0.5)) +
+  theme_minimal()
+
+rm(dfExtreme1, dfLev, dfPlot)
+
+
+# counterfactual HHI ----
+dfHhi <- dfExtreme %>%
+  group_by(anio, simGroups, sector) %>%
+  summarise(newHhi = mean(hhi_alt, na.rm = T)) %>%
+  filter(simGroups == 'Percentil > 80')
+
+dfExtreme1 <- full_join(dfExtreme,dfHhi[,c(1,3,4)], by = c('anio', 'sector'))
+
+dfExtreme1 <- dfExtreme1 %>%
+  mutate(
+    counterHhi = # bottom change lev at top's rate
+      Bomega_acf_sample*omega_acf_sample +
+      Bwdev_expo_s*wdev_expo_s +
+      Bwdev_impo_s*wdev_impo_s +
+      Bexpo_fob*expo_fob +
+      Bimpo_fob*impo_fob +
+      BBlev_bg*Blev_bg +
+      BLiqui_bg*Liqui_bg +
+      Btasap*tasap +
+      Bduracion_meses2p*duracion_meses2p +
+      Bratio1_bg*ratio1_bg +
+      Bhhi_alt*newHhi +
+      Bsi_alt*si_alt +
+      Bventas_sec*ventas_sec_1 +
+      B2005*a2005 +
+      B2006*a2006 +
+      B2007*a2007 +
+      B2008*a2008 +
+      B2009*a2009 +
+      B2010*a2010 +
+      B2011*a2011 +
+      B2012*a2012 +
+      B2013*a2013 +
+      fixed_effect +
+      Bcte
+  )
+
+dfPlot <- dfExtreme1 %>%
+  group_by(anio, simGroups) %>%
+  summarise(
+    counterHhi = mean(counterHhi*active, na.rm = T),
+  )%>%
+  group_by(simGroups) %>%
+  mutate(
+    counterHhiIndex = 100 + (counterHhi-counterHhi[anio==2005])/counterHhi,
+  )
+
+ggplot(dfPlot, aes(x=anio, y=counterHhiIndex)) +
+  geom_line(aes(color=simGroups), size= 1.1) +
+  scale_y_continuous(limits = c(99, 100.6), breaks = seq(from = 99,to = 100.6,by=0.5)) +
+  theme_minimal()
+
+rm(dfExtreme1, dfHhi, dfPlot)
+
+# counterfactual tasap ----
+
+dftasa<- dfExtreme %>%
+  group_by(anio, simGroups, sector) %>%
+  summarise(newtasa = mean(tasap, na.rm = T)) %>%
+  filter(simGroups == 'Percentil > 80')
+
+dfExtreme1 <- full_join(dfExtreme,dftasa[,c(1,3,4)], by = c('anio', 'sector'))
+
+dfExtreme1$newtasa[dfExtreme$simGroups=='Percentil > 80'] <- dfExtreme$tasap[dfExtreme$simGroups=='Percentil > 80']
+
+dfExtreme1 <- dfExtreme1 %>%
+  mutate(
+    counterTasa = # bottom change lev at top's rate
+      Bomega_acf_sample*omega_acf_sample +
+      Bwdev_expo_s*wdev_expo_s +
+      Bwdev_impo_s*wdev_impo_s +
+      Bexpo_fob*expo_fob +
+      Bimpo_fob*impo_fob +
+      BBlev_bg*Blev_bg +
+      BLiqui_bg*Liqui_bg +
+      Btasap*newtasa +
+      Bduracion_meses2p*duracion_meses2p +
+      Bratio1_bg*ratio1_bg +
+      Bhhi_alt*hhi_alt +
+      Bsi_alt*si_alt +
+      Bventas_sec*ventas_sec_1 +
+      B2005*a2005 +
+      B2006*a2006 +
+      B2007*a2007 +
+      B2008*a2008 +
+      B2009*a2009 +
+      B2010*a2010 +
+      B2011*a2011 +
+      B2012*a2012 +
+      B2013*a2013 +
+      fixed_effect +
+      Bcte
   )
 
 
-# dataframe for comparing 
-dfPlot2 <- df%>%
-  group_by(anio) %>%
+dfPlot <- dfExtreme1 %>%
+  group_by(anio, simGroups) %>%
   summarise(
-    baseline = mean(baseline, na.rm =  T),
-    baselineEntry = mean(baseline*active, na.rm = T),
-    observed = mean(pn_bruta, na.rm = T)
-    ) %>%
+    counterTasa = mean(counterTasa*active, na.rm = T),
+  )%>%
+  group_by(simGroups) %>%
   mutate(
-    baseline = 100 + (baseline - baseline[anio==2005])/baseline,
-    baselineEntry = 100 + (baselineEntry - baselineEntry[anio==2005])/baselineEntry,
-    observed = 100 + (observed - observed[anio==2005])/observed
-    ) %>%
-  gather(types, revenue, baseline:observed)
+    counterTasaIndex = 100 + (counterTasa-counterTasa[anio==2005])/counterTasa,
+  )
+
+
+ggplot(dfPlot, aes(x=anio, y=counterTasaIndex)) +
+  geom_line(aes(color=simGroups), size= 1.1) +
+  scale_y_continuous(limits = c(99, 100.6), breaks = seq(from = 99,to = 100.6,by=0.5)) +
+  theme_minimal()
+
+
+rm(dfExtreme1,dfPlot, dftasa)
+
+
+# counterfactual exportaciones ---- 
+
+dfExpo<- dfExtreme %>%
+  group_by(anio, simGroups, sector) %>%
+  summarise(newExpo = mean(expo_fob, na.rm = T)) %>%
+  filter(simGroups == 'Percentil > 80')
+
+dfExtreme1 <- full_join(dfExtreme,dfExpo[,c(1,3,4)], by = c('anio', 'sector'))
+
+dfExtreme1$newExpo[dfExtreme1$simGroups=='Percentil > 80'] <- dfExtreme$expo_fob[dfExtreme$simGroups=='Percentil > 80']
+
+dfExtreme1 <- dfExtreme1 %>%
+  mutate(
+    counterExpo= # bottom change lev at top's rate
+      Bomega_acf_sample*omega_acf_sample +
+      Bwdev_expo_s*wdev_expo_s +
+      Bwdev_impo_s*wdev_impo_s +
+      Bexpo_fob*newExpo +
+      Bimpo_fob*impo_fob +
+      BBlev_bg*Blev_bg +
+      BLiqui_bg*Liqui_bg +
+      Btasap*tasap +
+      Bduracion_meses2p*duracion_meses2p +
+      Bratio1_bg*ratio1_bg +
+      Bhhi_alt*hhi_alt +
+      Bsi_alt*si_alt +
+      Bventas_sec*ventas_sec_1 +
+      B2005*a2005 +
+      B2006*a2006 +
+      B2007*a2007 +
+      B2008*a2008 +
+      B2009*a2009 +
+      B2010*a2010 +
+      B2011*a2011 +
+      B2012*a2012 +
+      B2013*a2013 +
+      fixed_effect +
+      Bcte
+  )
+
+
+dfPlot <- dfExtreme1 %>%
+  group_by(anio, simGroups) %>%
+  summarise(
+    counterExpo = mean(counterExpo*active, na.rm = T),
+  )%>%
+  group_by(simGroups) %>%
+  mutate(
+    counterExpoIndex = 100 + (counterExpo-counterExpo[anio==2005])/counterExpo,
+  )
+
+
+ggplot(dfPlot, aes(x=anio, y=counterExpoIndex)) +
+  geom_line(aes(color=simGroups), size= 1.1) +
+  scale_y_continuous(limits = c(99, 100.6), breaks = seq(from = 99,to = 100.6,by=0.5)) +
+  theme_minimal()
+
+
+rm(dfExtreme1,dfPlot, dfExpo)
+
+
+
+
+# counterfactual importaciones ---- 
+
+dfImpo<- dfExtreme %>%
+  group_by(anio, simGroups, sector) %>%
+  summarise(newImpo = mean(impo_fob, na.rm = T)) %>%
+  filter(simGroups == 'Percentil > 80')
+
+dfExtreme1 <- full_join(dfExtreme,dfImpo[,c(1,3,4)], by = c('anio', 'sector'))
+
+dfExtreme1$newImpo[dfExtreme1$simGroups=='Percentil > 80'] <- dfExtreme$impo_fob[dfExtreme$simGroups=='Percentil > 80']
+
+dfExtreme1 <- dfExtreme1 %>%
+  mutate(
+    counterImpo= # bottom change lev at top's rate
+      Bomega_acf_sample*omega_acf_sample +
+      Bwdev_expo_s*wdev_expo_s +
+      Bwdev_impo_s*wdev_impo_s +
+      Bexpo_fob*expo_fob +
+      Bimpo_fob*newImpo +
+      BBlev_bg*Blev_bg +
+      BLiqui_bg*Liqui_bg +
+      Btasap*tasap +
+      Bduracion_meses2p*duracion_meses2p +
+      Bratio1_bg*ratio1_bg +
+      Bhhi_alt*hhi_alt +
+      Bsi_alt*si_alt +
+      Bventas_sec*ventas_sec_1 +
+      B2005*a2005 +
+      B2006*a2006 +
+      B2007*a2007 +
+      B2008*a2008 +
+      B2009*a2009 +
+      B2010*a2010 +
+      B2011*a2011 +
+      B2012*a2012 +
+      B2013*a2013 +
+      fixed_effect +
+      Bcte
+  )
+
+
+dfPlot <- dfExtreme1 %>%
+  group_by(anio, simGroups) %>%
+  summarise(
+    counterImpo = mean(counterImpo*active, na.rm = T),
+  )%>%
+  group_by(simGroups) %>%
+  mutate(
+    counterImpoIndex = 100 + (counterImpo-counterImpo[anio==2005])/counterImpo,
+  )
+
+
+ggplot(dfPlot, aes(x=anio, y=counterImpoIndex)) +
+  geom_line(aes(color=simGroups), size= 1.1) +
+  scale_y_continuous(limits = c(99, 100.6), breaks = seq(from = 99,to = 100.6,by=0.5)) +
+  theme_minimal()
+
+
+rm(dfExtreme1,dfPlot, dfExpo)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ PITCHME.md
@@ -0,0 +1,55 @@
+# Draft ESPE
+### Escenarios contrafactuales con "entrada"" y "salida""
+
+
+---
+
+### Ajuste: observado y predicho
+
+![](https://i.imgur.com/Hq6QR8L.png)
+
+---
+
+
+### Contrafactual: Apalancamiento
+
+Las firmas con peor desempeño se endeudan al ritmo de las mejores
+
+![](https://i.imgur.com/6PnRyEq.png)
+
+---
+
+
+### Contrafactual: Tasa
+
+Las firmas con peor desempeño acceden a las tasas promedio de las mejores
+
+![](https://i.imgur.com/OAeTBMF.png)
+
+---
+
+
+### Contrafactual: Competencia
+
+Se le impone a los sectores de las peores firmas el hhi de las mejores
+
+
+![](https://i.imgur.com/WazmDR9.png)
+
+---
+
+
+### Contrafactual: Exportaciones
+
+Las peores firmas exportan la proporción de su producción que exportan las mejores
+
+![](https://i.imgur.com/udQq4km.png)
+
+---
+
+
+### Contrafactual: Importaciones
+
+Las peores firmas Importan la proporción de su producció que exportan las mejores
+
+![](https://i.imgur.com/Up6G5Ad.png)
